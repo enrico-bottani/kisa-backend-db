ba7f261071fd0baf5f897613086fb515b303cb38
Enrico <enrico.bottani@hotmail.com>
2022-03-03 18:42:04 +0100

    AnswerableItemController

---
+++ src/main/java/team/exploding/kisabackenddb/controller/AnswerableItemController.java
@@ -0,0 +1,25 @@
+package team.exploding.kisabackenddb.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import team.exploding.kisabackenddb.dto.epage.MRCAnswerableItemDTO;
+import team.exploding.kisabackenddb.model.assignables.mrc.MRCAnswerableItem;
+import team.exploding.kisabackenddb.service.MRCAnswerableItemService;
+
+@Controller
+public class AnswerableItemController {
+    @Autowired
+    MRCAnswerableItemService mrcAnswerableItemService;
+    @CrossOrigin
+    @PutMapping(value = "/answerable_item/{id}.json")
+    public ResponseEntity<MRCAnswerableItemDTO> editById(@PathVariable(name = "id") long id, @RequestBody MRCAnswerableItem mrcAnswItem){
+        return ResponseEntity.of(mrcAnswerableItemService.updateById(id, mrcAnswItem));
+    }
+    @CrossOrigin
+    @GetMapping(value = "/answerable_item/{id}.json")
+    public ResponseEntity<MRCAnswerableItem> findById(@PathVariable(name = "id") long id){
+        return ResponseEntity.of(mrcAnswerableItemService.findById(id));
+    }
+}
---
+++ src/main/java/team/exploding/kisabackenddb/service/MRCAnswerableItemService.java
@@ -0,0 +1,35 @@
+package team.exploding.kisabackenddb.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.exploding.kisabackenddb.dto.epage.MRCAnswerableItemDTO;
+import team.exploding.kisabackenddb.mapper.editor.MRCAnswerableItemMapper;
+import team.exploding.kisabackenddb.model.assignables.mrc.MRCAnswerableItem;
+import team.exploding.kisabackenddb.repository.MRCAnswerableItemRepository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Service
+public class MRCAnswerableItemService {
+    @Autowired
+    MRCAnswerableItemRepository answerableItemRepository;
+    @Autowired
+    MRCAnswerableItemMapper mrcAnswerableItemMapper;
+    @Transactional
+    public Optional<MRCAnswerableItem> findById(long id) {
+        return  answerableItemRepository.findById(id);
+    }
+
+    @Transactional
+    public Optional<MRCAnswerableItemDTO> updateById(long id, MRCAnswerableItem mrcAnswItem) {
+        var answer = answerableItemRepository.findById(id);
+        if (answer.isEmpty()) {
+            return answer.map(mrcAnswerableItemMapper::map);
+        }
+        answer.get().setChoice(mrcAnswItem.getChoice());
+        answerableItemRepository.save(answer.get());
+        return answer.map(mrcAnswerableItemMapper::map);
+    }
+}
