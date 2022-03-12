7d913261082f38107b82b9ea6c69ee49cb70022c
Enrico <enrico@cryms.com>
2022-03-12 08:04:38 +0100

    ExerciseControllerTest.java

--- src/test/java/team/exploding/kisabackenddb/controller/ExerciseControllerTest.java
+++ src/test/java/team/exploding/kisabackenddb/controller/ExerciseControllerTest.java
@@ -1,17 +1,25 @@
 package team.exploding.kisabackenddb.controller;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import team.exploding.kisabackenddb.dto.exercise.ExerciseDTO;
 import team.exploding.kisabackenddb.service.ExerciseService;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
 
 @WebMvcTest(controllers = ExerciseController.class)
 public class ExerciseControllerTest {
@@ -26,8 +34,14 @@ public class ExerciseControllerTest {
     @Test
     @DisplayName("Should List all the exercises when making GET request to endpoint - /exercises.json")
     public void shouldListAllExercises() throws Exception{
-        mockMvc.perform(MockMvcRequestBuilders.get("/exercises.json")).andExpect(MockMvcResultMatchers
-                .status().is(200));
+
+        Mockito.when(exerciseService.finAll()).thenReturn(List.of(ExerciseDTO.builder().id(123L).build()));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/exercises.json"))
+                .andExpect(MockMvcResultMatchers.status().is(200))
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", Matchers.is(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id",Matchers.is(123)));
     }
 }
 //http://localhost:8081/exercises.json
